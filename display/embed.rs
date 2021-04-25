e4b46152a3d130fe2e52ac787059e556a4959f4e
Louis Birla <louis.birla@gmail.com>
2021-04-24 22:34:23 -0500

    feat: add habit block display & features

---
+++ display/embed.rs
@@ -0,0 +1,85 @@
+use crate::blocks::habit_block::HabitBlock;
+use block_tools::{
+	auth::{
+		optional_token, optional_validate_token,
+		permissions::{has_perm_level, PermLevel},
+	},
+	blocks::Context,
+	display_api::component::{
+		atomic::text::TextComponent,
+		interact::link::LinkComponent,
+		layout::{
+			card::CardComponent,
+			stack::{AlignXOptions, StackComponent},
+		},
+		DisplayComponent,
+	},
+	models::Block,
+	LoopError,
+};
+
+impl HabitBlock {
+	pub fn handle_embed_display(
+		block: &Block,
+		context: &Context,
+	) -> Result<DisplayComponent, LoopError> {
+		let conn = &context.conn()?;
+		let user_id = optional_validate_token(optional_token(context))?;
+
+		let Self {
+			name,
+			description,
+			impact,
+			score,
+			streak,
+		} = Self::from_id(block.id, user_id, conn)?;
+
+		let mut left_col = StackComponent::vertical();
+		let mut middle_col = StackComponent::vertical();
+		let mut right_col = StackComponent {
+			align_x: Some(AlignXOptions::Right),
+			..StackComponent::vertical()
+		};
+
+		let name = name
+			.and_then(|block| block.block_data)
+			.unwrap_or_else(|| "Untitled Habit".into());
+		let text = TextComponent {
+			bold: Some(true),
+			..TextComponent::new(name)
+		};
+		let link = LinkComponent {
+			app_path: Some(format!("/b/{}", block.id)),
+			no_style: Some(true),
+			..LinkComponent::new(text)
+		};
+		middle_col.push(link);
+
+		if let Some(desc) = Self::description(&description, false) {
+			middle_col.push(desc)
+		}
+
+		if let Some(user_id) = user_id {
+			if has_perm_level(user_id, block, PermLevel::Edit) {
+				let buttons_stack = Self::buttons_stack(impact, block.id);
+				right_col.push(buttons_stack);
+			}
+		}
+
+		right_col.push(Self::streak(streak));
+
+		left_col.push(Self::score_circle(score, block));
+
+		let mut content = StackComponent::horizontal();
+		content.push(left_col);
+		content.push(middle_col);
+		content.push(right_col);
+
+		Ok(CardComponent {
+			color: block.color.clone(),
+			content: box content.into(),
+			header: None,
+		}
+		.into())
+	}
+}
--- mod.rs
+++ mod.rs
@@ -4,17 +4,24 @@ use block_tools::{
 		component::{atomic::icon::Icon, layout::card::CardComponent, DisplayComponent},
 		CreationObject, DisplayObject,
 	},
-	models::{Block},
+	models::Block,
 	LoopError,
 };
+mod display;
+mod from_id;
+mod methods;
+mod name;
 
 pub const BLOCK_NAME: &str = "habit";
 
+#[derive(Default)]
 /// A block type that can be thought of like a sort of folder.
 pub struct HabitBlock {
 	pub name: Option<Block>,
 	pub description: Option<Block>,
-	pub items: Vec<Block>,
+	pub impact: Option<Block>,
+	pub streak: Option<Block>,
+	pub score: Option<Block>,
 }
 
 impl BlockType for HabitBlock {
@@ -31,29 +38,24 @@ impl BlockType for HabitBlock {
 	}
 
 	fn block_name(block: &Block, context: &Context) -> Result<String, LoopError> {
-		todo!()
-		// Self::handle_block_name(block, context)
+		Self::handle_block_name(block, context)
 	}
 
 	fn page_display(block: &Block, context: &Context) -> Result<DisplayObject, LoopError> {
-		todo!()
-		// Self::handle_page_display(block, context)
+		Self::handle_page_display(block, context)
 	}
 
 	fn embed_display(block: &Block, context: &Context) -> DisplayComponent {
-		todo!()
-		// Self::handle_embed_display(block, context)
-		// 	.unwrap_or_else(|e| CardComponent::error_card(e).into())
+		Self::handle_embed_display(block, context)
+			.unwrap_or_else(|e| CardComponent::error_card(e).into())
 	}
 
 	fn create_display(context: &Context, user_id: i32) -> Result<CreationObject, LoopError> {
-		todo!()
-		// Self::handle_create_display(context, user_id)
+		Self::handle_create_display(context, user_id)
 	}
 
 	fn create(input: String, context: &Context, user_id: i32) -> Result<Block, LoopError> {
-		todo!()
-		// Self::handle_create_raw(input, context, user_id)
+		Self::handle_create_raw(input, context, user_id)
 	}
 
 	fn method_delegate(
@@ -62,12 +64,10 @@ impl BlockType for HabitBlock {
 		block_id: i64,
 		args: String,
 	) -> Result<Block, LoopError> {
-		todo!()
-		// Self::handle_method_delegate(context, name, block_id, args)
+		Self::handle_method_delegate(context, name, block_id, args)
 	}
 
 	fn visibility_update(context: &Context, block_id: i64, public: bool) -> Result<(), LoopError> {
-		todo!()
-		// Self::handle_visibility_update(context, block_id, public)
+		Self::handle_visibility_update(context, block_id, public)
 	}
 }
