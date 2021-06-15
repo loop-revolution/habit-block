6b5e3beb7f583b9bdd3477c38e424771c701fb36
Louis Birla <louis.birla@gmail.com>
2021-06-14 20:45:01 -0500

    fix: make habit block presentable on mobile

--- display/embed.rs
+++ display/embed.rs
@@ -54,36 +54,66 @@ impl HabitBlock {
 			no_style: Some(true),
 			..LinkComponent::new(text)
 		};
-		middle_col.push(link);
+		middle_col.push(link.clone());
+
+		let mut desc_component = None;
 
 		if let Some(desc) = Self::description(&description, false) {
-			middle_col.push(desc)
+			middle_col.push(desc.clone());
+			desc_component = Some(desc);
 		}
 
 		let mut detached_menu = None;
+		let mut action_buttons = None;
 
 		if let Some(user_id) = user_id {
 			if has_perm_level(user_id, block, PermLevel::Edit) {
 				let buttons_stack = Self::buttons_stack(impact, block.id);
-				right_col.push(buttons_stack);
+				right_col.push(buttons_stack.clone());
+				action_buttons = Some(buttons_stack);
 				let mut menu = MenuComponent::from_block(block, user_id);
 				menu.load_comments(conn)?;
 				detached_menu = Some(DetachedMenu::bottom_right(menu));
 			}
 		}
+		let mut mobile_first_row = StackComponent::horizontal();
+		let score_circle = Self::score_circle(score, block);
+		let streak = Self::streak(streak);
 
-		right_col.push(Self::streak(streak));
+		mobile_first_row.push(score_circle.clone());
 
-		left_col.push(Self::score_circle(score, block));
+		let mut mobile_info = StackComponent::vertical();
+		mobile_info.push(link);
+		mobile_info.push(streak.clone());
+		mobile_first_row.push(mobile_info);
+
+		right_col.push(streak);
+		left_col.push(score_circle);
 
 		let mut content = StackComponent::horizontal();
 		content.push(left_col);
 		content.push(middle_col);
 		content.push(right_col);
 
+		let mut mobile_content = StackComponent::vertical();
+		mobile_content.push(mobile_first_row);
+		if let Some(btns) = action_buttons {
+			mobile_content.push(btns);
+		}
+		if let Some(desc) = desc_component {
+			mobile_content.push(desc);
+		}
+
+		let mobile_card = CardComponent {
+			color: block.color.clone(),
+			detached_menu: detached_menu.clone(),
+			..CardComponent::new(mobile_content)
+		};
+
 		Ok(CardComponent {
 			color: block.color.clone(),
 			detached_menu,
+			mobile_override: Some(box mobile_card),
 			..CardComponent::new(content)
 		}
 		.into())
